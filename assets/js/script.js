176ff3ddb09cff3ee6f47fc74e98daf2fc1405f7
RogerLi818 <jackliteng@yahoo.com>
2022-04-08 23:54:04 -0700

    fix bug to seperate two click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,40 +46,10 @@ var saveTasks = function() {
 };
 
 $(".list-group").on("click","p", function(){
-  $(".list-group").on("blur","textarea",function(){
-    //get the textarea's current value/text
-    var text=$(this)
-    .val()
-    .trim();
-
-    //get the parent ul's id attribute
-    var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-","");
-
-    //get the task's position in the list of other li elements
-    var index =$(this)
-    .closest(".list-group-item")
-    .index();
-
-    tasks[status][index].text = text;
-    saveTasks();
-
-    //recreate p element
-    var taskP=$("<p>")
-    .addClass("m-1")
-    .text(text);
-
-    //replace textarea with p element
-    $(this).replaceWith(taskP);
-  });
-
 
   var text =$(this)
   .text()
-  .trim();
-  
+  .trim();  
 
   var textInput=$("<textarea>")
   .addClass("from-control")
@@ -89,37 +59,38 @@ $(".list-group").on("click","p", function(){
   textInput.trigger("focus");
 })
 
+$(".list-group").on("blur","textarea",function(){
+  //get the textarea's current value/text
+  var text=$(this)
+  .val()
+  .trim();
+
+  //get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-","");
+
+  //get the task's position in the list of other li elements
+  var index =$(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  //recreate p element
+  var taskP=$("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  //replace textarea with p element
+  $(this).replaceWith(taskP);
+});
+
 //due date was clicked
 $(".list-group").on("click","span", function(){
-  $(".list-group").on("blur","input[type='text']",function(){
-    //get current text
-    var date = $(this)
-    .val()
-    .trim();
-
-    //get the parent ul's id attribute
-    var status=$(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
-
-    //get the task's position in the list of other li elements
-    var index=$(this)
-    .closest(".list-group-item")
-    .index();
-
-    //update task in array and re-save to localstorage
-    tasks[status][index].date=date;
-    saveTasks();
-
-    //recreate span element with bootstrap classes
-    var taskSpan=$("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
-
-    //replace input with span element
-    $(this).replaceWith(taskSpan);
-  });
+  
 
   //get current text
   var date = $(this)
@@ -135,6 +106,36 @@ $(".list-group").on("click","span", function(){
   //automaticlly focus on new element
   dateInput.trigger("focus");
 })
+
+$(".list-group").on("blur","input[type='text']",function(){
+  //get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  //get the parent ul's id attribute
+  var status=$(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  //get the task's position in the list of other li elements
+  var index=$(this)
+  .closest(".list-group-item")
+  .index();
+
+  //update task in array and re-save to localstorage
+  tasks[status][index].date=date;
+  saveTasks();
+
+  //recreate span element with bootstrap classes
+  var taskSpan=$("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 
 // modal was triggered
